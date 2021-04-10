2e84623c92d4cbde53a7d5b96801d6f2da707ee6
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-04-10 13:43:54 -0400

    initial update to resubmit cmd

--- bin/faults-cmds/resubmit.js
+++ bin/faults-cmds/resubmit.js
@@ -1,29 +1,32 @@
+const { invokeLambda } = require('aws-lambda-stream');
+
+const { now, head, print, debug, digest, count } = require('../../lib/common');
+
 exports.command = 'resubmit [bucket] [prefix]'
 exports.describe = 'Resubmit the faults in [bucket] for [prefix]'
-
-const _ = require('highland');
-const now = require('moment')().utc();
 
 exports.builder = {
     bucket: {
         alias: 'b',
         describe: 'bucket containing the faults'
     },
-    stream: {
-        alias: 's',
-        describe: 'stream that delivered the faults - root prefix'
+    region: {
+        alias: 'r',
+        default: process.env.AWS_REGION || 'us-east-1',
+        describe: 'root prefix'
     },
     prefix: {
         alias: 'p',
-        default: `${now.format('YYYY')}/${now.format('MM')}/${now.format('DD')}/`
+        default: `${now.format('YYYY')}/${now.format('MM')}/${now.format('DD')}/`,
+        describe: 'folder of faults to retrieve'
     },
-    function: {
-        alias: 'f',
-        describe: 'function to resubmit the events to'
-    },
-    qualifier: {
-        default: '$LATEST'
-    },
+    // // function: {
+    // //     alias: 'f',
+    // //     describe: 'function to resubmit the events to'
+    // // },
+    // qualifier: {
+    //     default: '$LATEST'
+    // },
     dry: {
         default: true,
         type: 'boolean'
@@ -40,70 +43,74 @@ exports.builder = {
     window: {
         default: 500
     },
-    region: {
-        alias: 'r',
-        default: process.env.AWS_REGION || 'us-east-1'
-    },
-    source: {
-        alias: 'sp',
-        description: 'source account profile',
-        default: process.env.AWS_PROFILE || 'default'
-    },
-    target: {
-        alias: 'tp',
-        description: 'target account profile',
-        default: process.env.AWS_PROFILE || 'default'
-    },
+    // target: {
+    //     alias: 'tp',
+    //     description: 'target account profile',
+    //     default: process.env.AWS_PROFILE || 'default'
+    // },
 }
 
 exports.handler = (argv) => {
-    const common = require('../../lib/common')(argv);
+    print(argv);
+    head(argv)
 
-    common.print(argv);
+        // .map(obj => {
+        //     const fault = obj.obj;
 
-    common.s3.paginate()
-        .flatMap(obj => common.s3.get(obj))
-        .map(obj => {
-            const fault = obj.obj;
+        //     const newKinesisEvent = {
+        //         key: obj.key,
+        //         functionName: fault.tags.functionName,
 
-            const newKinesisEvent = {
-                key: obj.key,
-                functionName: fault.tags.functionName,
-                records: []
-            };
+        //         // TODO pipeline ???
 
-            const uows = Array.isArray(fault.uow) ? fault.uow : [fault.uow];
+        //         records: []
+        //     };
 
-            uows.forEach(uow => {
-                if (uow.batch) {
-                    newKinesisEvent.records = newKinesisEvent.records.concat(fault.uow.batch.map(b => b.record));
-                }
+        //     const uows = Array.isArray(fault.uow) ? fault.uow : [fault.uow];
 
-                if (uow.record) {
-                    newKinesisEvent.records.push(fault.uow.record);
-                }
-            });
+        //     uows.forEach(uow => {
+        //         if (uow.batch) {
+        //             newKinesisEvent.records = newKinesisEvent.records.concat(fault.uow.batch.map(b => b.record));
+        //         }
 
-            if (newKinesisEvent.records.length === 0) {
-                const e = new Error('No records in: ' + obj.key);
-                e.fault = fault;
-                throw e;
-            }
+        //         if (uow.record) {
+        //             newKinesisEvent.records.push(fault.uow.record);
+        //         }
+        //     });
 
-            return newKinesisEvent;
-        })
-        .tap(common.print)
+        //     if (newKinesisEvent.records.length === 0) {
+        //         const e = new Error('No records in: ' + obj.key);
+        //         e.fault = fault;
+        //         throw e;
+        //     }
 
-        .ratelimit(argv.rate, argv.window)
+        //     return newKinesisEvent;
+        // })
+        // .tap(debug)
 
-        .map(batch => common.lambda.invoke(batch))
-        .parallel(argv.parallel)
-        .tap(common.print)
+        // .map((uow => {
+
+        //     return {
+        //         ...uow,
+        //         invokeRequest: {
+        //             FunctionName: 'helloworld',
+        //         },
+        //     };
+        // }))
+
+        // .ratelimit(argv.rate, argv.window)
+
+
+        // TODO params
+        // .through(invokeLambda({ parallel: argv.parallel }))
+
+        // .map(batch => common.lambda.invoke(batch))
+        // .parallel(argv.parallel)
+        .tap(debug)
 
         .collect()
-        .tap(d => console.log('Count: %s', d.length))
+        .tap(collected => console.log('Count: %s', collected.length))
 
-        .errors(common.print)
-        .done(() => { })
-        ;
+        .errors(console.log)
+        .done(() => { });
 }
