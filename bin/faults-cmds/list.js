bf76a9b582a764a9766d3511ef01b6fe6fbc397f
John Gilbert <1082126+jgilbert01@users.noreply.github.com>
2021-04-10 11:31:52 -0400

    updated ls cmds

--- bin/faults-cmds/list.js
+++ bin/faults-cmds/list.js
@@ -1,44 +1,37 @@
+const { now, list, print } = require('../../lib/common');
+
 exports.command = 'ls [bucket] [prefix]'
 exports.describe = 'List the fault files in [bucket] for [prefix]'
-
-const now = require('moment')().utc();
 
 exports.builder = {
     bucket: {
         alias: 'b',
         describe: 'bucket containing the faults'
     },
-    stream: {
-        alias: 's',
-        describe: 'stream that delivered the faults - root prefix'
+    region: {
+        alias: 'r',
+        default: process.env.AWS_REGION || 'us-east-1',
+        describe: 'root prefix'
     },
     prefix: {
         alias: 'p',
-        default: `${now.format('YYYY')}/${now.format('MM')}/${now.format('DD')}/`
+        default: `${now.format('YYYY')}/${now.format('MM')}/${now.format('DD')}/`,
+        describe: 'folder of objects to retrieve'
     },
-    region: {
-        alias: 'r',
-        default: process.env.AWS_REGION || 'us-east-1'
+    parallel: {
+        default: 16
     },
-    source: {
-        alias: 'sp',
-        description: 'source account profile',
-        default: process.env.AWS_PROFILE || 'default'
-    }
 }
 
 exports.handler = (argv) => {
-    const common = require('../../lib/common')(argv);
+    print(argv);
+    list(argv)
 
-    common.print(argv);
-
-    common.s3.paginate()
-        .tap(common.print)
+        .tap(uow => print(uow.listResponse.Content))
 
         .collect()
-        .tap(d => console.log('Count: %s', d.length))
+        .tap(collected => console.log('Count: %s', collected.length))
 
-        .errors(common.print)
-        .done(() => {})
-        ;
+        .errors(console.log)
+        .done(() => { });
 }
